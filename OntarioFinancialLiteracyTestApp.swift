73afda5cb22ae16d5bb93c86343ebd46815eb28a
talhai21 <italha.inam@gmail.com>
2025-01-07 14:16:13 -0500

    Create OntarioFinancialLiteracyTestApp.swift

---
+++ OntarioFinancialLiteracyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OntarioFinancialLiteracyTestApp.swift
+//  OntarioFinancialLiteracyTest
+//
+//  Created by Chukwuka Okwusiuno on 2024-12-22.
+//
+
+import SwiftUI
+
+@main
+struct OntarioFinancialLiteracyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
